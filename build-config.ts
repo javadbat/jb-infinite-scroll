881cda7a8ca86485382fb3089a7da89ee51955ee
javadbat <javadbat@gmail.com>
2025-02-01 15:37:16 +0330

    feat: add build config

---
+++ build-config.ts
@@ -0,0 +1,27 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [
+  {
+    name: "jb-infinite-scroll",
+    path: "./lib/jb-infinite-scroll.ts",
+    outputPath: "./dist/jb-infinite-scroll.js",
+    umdName: "JBInfiniteScroll",
+    external: ["jb-loading"],
+    globals: {
+      "jb-loading": "JBLoading",
+    },
+  },
+];
+export const reactComponentList: ReactComponentBuildConfig[] = [
+  {
+    name: "jb-infinite-scroll-react",
+    path: "./react/lib/JBInfiniteScroll.tsx",
+    outputPath: "./react/dist/JBInfiniteScroll.js",
+    external: ["jb-infinite-scroll", "prop-types", "react"],
+    globals: {
+      react: "React",
+      "prop-types": "PropTypes",
+      "jb-infinite-scroll": "JBInfiniteScroll",
+    },
+  },
+];
