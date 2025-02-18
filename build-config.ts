d364f4dbabaf7eea545d7c8895475e69f5f2cce2
javadbat <javadbat@gmail.com>
2025-02-18 13:41:56 +0330

    add core dependancy

--- build-config.ts
+++ build-config.ts
@@ -6,9 +6,10 @@ export const webComponentList: WebComponentBuildConfig[] = [
     path: "./lib/jb-infinite-scroll.ts",
     outputPath: "./dist/jb-infinite-scroll.js",
     umdName: "JBInfiniteScroll",
-    external: ["jb-loading"],
+    external: ["jb-loading", "jb-core"],
     globals: {
       "jb-loading": "JBLoading",
+      "jb-core":"JBCore"
     },
   },
 ];
@@ -17,11 +18,13 @@ export const reactComponentList: ReactComponentBuildConfig[] = [
     name: "jb-infinite-scroll-react",
     path: "./react/lib/JBInfiniteScroll.tsx",
     outputPath: "./react/dist/JBInfiniteScroll.js",
-    external: ["jb-infinite-scroll", "prop-types", "react"],
+    external: ["jb-infinite-scroll", "prop-types", "react", "jb-core", "jb-core/react"],
     globals: {
       react: "React",
       "prop-types": "PropTypes",
       "jb-infinite-scroll": "JBInfiniteScroll",
+      "jb-core":"JBCore",
+      "JBCoreReact":"JBCoreReact"
     },
     umdName: "JBInfiniteScrollReact",
     dir: "./react"
